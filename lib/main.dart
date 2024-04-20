e699b9187a0633f73f1d07e8b5589e37dc8ffc3b
Namju Kim <cmsong111@naver.com>
2024-04-21 05:22:48 +0900

    Project: Add Firebase Dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:nakdong_river/firebase_options.dart';
 
 import 'presentation/views/my_home_page.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
